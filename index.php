2cc0c51eedc23ca47dd7f785abaa9a523dac4461
pavelzelenevich <52541334+pavelzelenevich@users.noreply.github.com>
2020-06-13 04:43:32 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,20 @@
 <?php
-//ini_set('display_errors','1');
-//ini_set('display_startup_errors', '1');
-//error_reporting(E_ALL);
-
 require_once('./pdo.php');
 require_once ('./contr.php');
 
 $copyresultarray = $resultLead;
 $dat = null;
+
 /** @var TabLead $item */
-foreach ($resultLead as $keyfirst => $item){
+foreach ($resultLead as $keyfirst => $item) {
     if($item->getDate() === $dat){
         continue;
     }
     $dat = $item->getDate();
     echo sprintf("| %s |", $item->getDayDate());
-
+    $count = 0;
     foreach ($copyresultarray as $key => $i) {
-        if(($i->getDate() === $dat) && ($count < 3)){
+        if(($i->getDate() === $dat) && ($count < 3)) {
             $count++;
             echo sprintf(" %s (%s hours)", $i->getUserName(), round($i->getHours(), 2));
             unset($copyresultarray[$key]);
